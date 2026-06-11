d2907412edc87b83123f75909f0d5c980a20fb35
Dilakshina <d.fernando3758@gmail.com>
2026-06-11 22:52:00 +0530

    updated code

--- IosProject1/ContentView.swift
+++ IosProject1/ContentView.swift
@@ -1,19 +1,9 @@
-//
-//  ContentView.swift
-//  IosProject1
-//
-//  Created by Dilakshina Fernando  on 2026-06-11.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Shane!")
+           Text("Update Project")
         }
         .padding()
     }
