755e4bd4e1c678eb3f3a44bcee950fab7d84dc38
Dilakshina <d.fernando3758@gmail.com>
2026-06-11 17:27:25 +0530

    Initial Commit

---
+++ IosProject1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IosProject1
+//
+//  Created by Dilakshina Fernando  on 2026-06-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Shane!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IosProject1/IosProject1App.swift
@@ -0,0 +1,17 @@
+//
+//  IosProject1App.swift
+//  IosProject1
+//
+//  Created by Dilakshina Fernando  on 2026-06-11.
+//
+
+import SwiftUI
+
+@main
+struct IosProject1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
